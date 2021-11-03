df65d6b2d6236efb30854d18170bfb8a48c955aa
ming <89713746+byeol0558@users.noreply.github.com>
2021-11-03 12:23:58 +0900

    qwe
    qwe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <h1>리액트 노드 프로젝트 테스트용</h1>
+      <h1>리액트 노드 프로젝트 동욱동욱</h1>
     </div>
   );
 }
